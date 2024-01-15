99c87cad49e2a057c01b67c7945a7c282eeb3317
connorgilchrist <155076339+connorgilchrist@users.noreply.github.com>
2024-01-15 17:53:30 +0000

    Add files via upload

--- PasswordManager.py
+++ PasswordManager.py
@@ -1,18 +1,33 @@
-master_pwd = input('Enter your master password')
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 def add():
     name = input('Account name: ')
     pwd = input('Password: ')
     #open file and automatically close it - append to it or create if file doesn't exist ('a')
     with open('passwords.txt', 'a') as f:
-        f.write(name + ' | ' + pwd + '\n')
+        f.write(name + ' | ' + fer.encrypt(pwd.encode()).decode() + '\n')
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
-    
-
+            data = line.rstrip()
+            user, passw = data.split('|')
+            print('User:', user, '| Password:', fer.decrypt(passw.encode()).decode())
 
 while True:
     mode = input('Would you like to add a new password or view an existing one? (view, add), press q to quit').lower()
